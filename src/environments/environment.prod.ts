5e0b9cd464c56d084da4695ccc004710d2781734
ilariaGalliano <ilaria.galliano7@gmail.com>
2025-11-25 18:33:02 +0100

    fixes pre deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiBase: 'https://api.tuodominio.com/api/v1'
+  apiBase: 'https://api.tuodominio.com/api/v1',
+  useMockApi: false, // Set to false for real API
+  mockLogin: true    
 };
