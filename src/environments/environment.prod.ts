308d08d1e790fa45429a35eb227a2a14458e1d7e
ilariaGalliano <ilaria.galliano7@gmail.com>
2025-12-14 18:14:40 +0100

    apiBase updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBase: 'https://kids-calendar-api.vercel.app/api/v1',
+  apiBase: 'https://kids-calendar-api.vercel.app',
   useMockApi: false, // Set to false for real API
   mockLogin: true    
 };
