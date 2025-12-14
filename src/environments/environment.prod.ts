a79ca5d8834a719efdad2000c36a2a711a51d65f
ilariaGalliano <ilaria.galliano7@gmail.com>
2025-12-14 18:10:38 +0100

    fixed apiBase to connect FE and BE

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBase: 'https://api.tuodominio.com/api/v1',
+  apiBase: 'https://kids-calendar-api.vercel.app/api/v1',
   useMockApi: false, // Set to false for real API
   mockLogin: true    
 };
